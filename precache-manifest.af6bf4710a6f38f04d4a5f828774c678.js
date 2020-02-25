9c4055abe515d0e78fe5a2710bac1fa1e9fafeee
ahmedfarrag9090 <farrag9090@gmail.com>
2020-02-25 19:40:52 +0200

    Updates

---
+++ precache-manifest.af6bf4710a6f38f04d4a5f828774c678.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71bb19fc197b5cbc81b558899ed964b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e3427c63faddf78875c5",
+    "url": "/todolist/static/css/main.ecdb354b.chunk.css"
+  },
+  {
+    "revision": "25d01e4de46183aa4c71",
+    "url": "/todolist/static/js/2.c5504464.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.c5504464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3427c63faddf78875c5",
+    "url": "/todolist/static/js/main.dda041b4.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  },
+  {
+    "revision": "435183d2938479cc977d4032f2010ac5",
+    "url": "/todolist/static/media/Yagada3.435183d2.jpg"
+  },
+  {
+    "revision": "37e35bbc20aca23707ced3c9b95454fa",
+    "url": "/todolist/static/media/oldwallpaper1.37e35bbc.jpg"
+  },
+  {
+    "revision": "111963bfc6e401a6b72e06c42602acdd",
+    "url": "/todolist/static/media/paper.111963bf.png"
+  }
+]);
