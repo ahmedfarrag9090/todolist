f8e7a9d1fbcd9c153cf8ec9d28d375036e93f4b0
ahmedfarrag9090 <farrag9090@gmail.com>
2020-04-10 23:55:51 +0200

    Use Refs

--- src/components/list-form/list-form.js
+++ src/components/list-form/list-form.js
@@ -3,12 +3,14 @@ import './list-form.css'
 
 export default class Form extends Component{
 
-    state={
-        taskName: ''
+    state= {
+        taskName: '',
+        txtInp: React.createRef()
     }
 
     handleChange = (e) => {
-        e.target.style.backgroundColor = 'transparent';
+        const inp = this.state.txtInp;
+        inp.current.style.backgroundColor = 'transparent';
         this.setState({
             [e.target.id]: e.target.value
         })
@@ -16,11 +18,12 @@ export default class Form extends Component{
       }
 
       handleSubmit = (e) => {
+        const inp = this.state.txtInp;
         e.preventDefault();
         if (this.state.taskName !== '') {
 
             this.props.addItem(this.state)
-        }else{e.target.querySelector('input').style.backgroundColor = 'salmon' ;}
+        }else{inp.current.style.backgroundColor = 'salmon' ;}
         this.setState({
             taskName:''
         })
@@ -33,7 +36,14 @@ export default class Form extends Component{
         return (
             <div className="form-container">
                 <form onSubmit={this.handleSubmit} >
-                    <input value={this.state.taskName} type="text" id="taskName" placeholder="Add your task" onChange={this.handleChange} autoComplete="off"/>
+                    <input
+                    ref={this.state.txtInp}
+                    value={this.state.taskName}
+                    type="text" id="taskName"
+                    placeholder="Add your task"
+                    onChange={this.handleChange}
+                    autoComplete="off"/>
+                    
                     <input type="submit" value="Add" />
                 </form>
             </div>
